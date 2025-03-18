548eab06c183bc3a3cefd7533d1672248e64ae2b
gonzalo334 <gonzalogarciame2005@gmail.com>
2025-03-18 18:07:21 +0100

    version2

--- src/Lab2_6_CV.py
+++ src/Lab2_6_CV.py
@@ -44,26 +44,30 @@ def cross_validation(model, X, y, nFolds):
         # Implement Leave One Out CV
         nFolds = X.shape[0]
 
-    # TODO: Calculate fold_size based on the number of folds
-    fold_size = None
+    # Calculate fold_size based on the number of folds
+    fold_size = X.shape[0] // nFolds
 
-    # TODO: Initialize a list to store the accuracy values of the model for each fold
+    # Initialize a list to store the accuracy values of the model for each fold
     accuracy_scores = []
 
     for i in range(nFolds):
-        # TODO: Generate indices of samples for the validation set for the fold
-        valid_indices = None
+        # Generate indices of samples for the validation set for the fold
+        valid_indices = list(range(i * fold_size, (i + 1) * fold_size))
 
-        # TODO: Generate indices of samples for the training set for the fold
-        train_indices = None
+        # Generate indices of samples for the training set for the fold
+        train_indices = list(set(range(X.shape[0])) - set(valid_indices))
 
-        # TODO: Split the dataset into training and validation
-        X_train, X_valid = None, None
-        y_train, y_valid = None, None
+        # Split the dataset into training and validation
+        X_train, X_valid = X[train_indices], X[valid_indices]
+        y_train, y_valid = y[train_indices], y[valid_indices]
 
-        # TODO: Train the model with the training set
+        # Train the model with the training set
+        model.fit(X_train, y_train)
 
-        # TODO: Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+        # Calculate the accuracy of the model with the validation set and store it in accuracy_scores
+        accuracy_scores.append(model.score(X_valid, y_valid))
 
-    # TODO: Return the mean and standard deviation of the accuracy_scores
-    return None, None
+    # Return the mean and standard deviation of the accuracy_scores
+    mean_score = sum(accuracy_scores) / len(accuracy_scores)
+    std_score = (sum((x - mean_score) ** 2 for x in accuracy_scores) / len(accuracy_scores)) ** 0.5
+    return mean_score, std_score
